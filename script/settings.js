07270bae7ee923da61231b86237c8bf8f2e9ea65
Henrique Prates (DELL - Aninha) <henrique.prates.br@gmail.com>
2025-01-08 14:57:57 -0300

    alteração da organização das pastas e restauração do jogo desenvolvido no primeiro ano

--- script/settings.js
+++
@@ -1,5 +0,0 @@
-const headerSettings = document.querySelector('.settings')
-const buttonSettings = document.querySelector('.settings_icon')
-buttonSettings.addEventListener('click', e => {
-    headerSettings.classList.toggle('settings--active')
-})
