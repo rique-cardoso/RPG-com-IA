9bb6349899bdda2a039d38952e8aa7b9426ca21a
Henrique Prates (DELL - Aninha) <henrique.prates.br@gmail.com>
2025-01-20 01:27:45 -0300

    função de continuação de jogo praticamente completa, navegaçaõ do jogo funcional e botões atualizando corretamente
    Próximos passos, adicionar renderização condicional com base no status do jogo

--- scripts/interface/renderResponse.js
+++ scripts/interface/renderResponse.js
@@ -1,41 +1,66 @@
-export function renderContext(contexto) {
-    const contextoSplitado = contexto.split(/\n/)
-    const opcoes = contextoSplitado.filter(e => /^\d/.test(e))
+const objCompletContext = {
+    mainContext: '',
+    presentOpcoes: '',
+    respostaCompleta: ''
+}
+export function criarElementosDom(){
+    // criação dos textos no DOM
     const main = document.querySelector('#response')
     const p = document.createElement('p')
     const op1 = document.createElement('p')
     const op2 = document.createElement('p')
     const op3 = document.createElement('p')
-    op1.textContent = opcoes[0]
-    op2.textContent = opcoes[1]
-    op3.textContent = opcoes[2]
-    p.textContent = contextoSplitado[0]
+    p.setAttribute('id', 'contexto')
+    op1.setAttribute('id', 'escolha1')
+    op2.setAttribute('id', 'escolha2')
+    op3.setAttribute('id', 'escolha3')
     main.appendChild(p)
     main.appendChild(op1)
     main.appendChild(op2)
     main.appendChild(op3)
-    const objCompletContext = {
-        mainContext: contextoSplitado[0],
-        presentOpcoes: opcoes,
-        respostaCompleta: contexto
-    }
-    return objCompletContext
-}
-export function renderButtons(){
-    const main = document.querySelector('#response')
+    
+    // criação dos botões no DOM
     const nav = document.createElement('nav')
     const btn1 = document.createElement("button")
     const btn2 = document.createElement("button")
     const btn3 = document.createElement("button")
     nav.classList.add('containerButtons')
     btn1.classList.add("btnOpcao")
-    btn2.classList.add("btnOpcao")
     btn3.classList.add("btnOpcao")
-    btn1.textContent = '1'
-    btn2.textContent = '2'
-    btn3.textContent = '3'
+    btn2.classList.add("btnOpcao")
+    btn1.setAttribute('id', 'botao1')
+    btn2.setAttribute('id', 'botao2')
+    btn3.setAttribute('id', 'botao3')
     main.appendChild(nav)
     nav.appendChild(btn1)
     nav.appendChild(btn2)
     nav.appendChild(btn3)
+}
+export function renderContext(contexto) {
+    const contextoSplitado = contexto.split(/\n/)
+    const opcoes = contextoSplitado.filter(e => /^\d/.test(e))
+    // const main = document.querySelector('#response')
+    const p = document.querySelector('#contexto')
+    const op1 = document.querySelector('#escolha1')
+    const op2 = document.querySelector('#escolha2')
+    const op3 = document.querySelector('#escolha3')
+    op1.textContent = opcoes[0]
+    op2.textContent = opcoes[1]
+    op3.textContent = opcoes[2]
+    p.textContent = contextoSplitado[0]
+    objCompletContext.mainContext = contextoSplitado[0]
+    objCompletContext.presentOpcoes = opcoes
+    objCompletContext.respostaCompleta = contextoSplitado
+}
+export function renderButtons(){
+    // const main = document.querySelector('#response')
+    const btn1 = document.querySelector('#botao1')
+    const btn2 = document.querySelector('#botao2')
+    const btn3 = document.querySelector('#botao3')
+    btn1.textContent = '1'
+    btn2.textContent = '2'
+    btn3.textContent = '3'
+}
+export function retornaObjetoAtual(){
+    return objCompletContext
 }
